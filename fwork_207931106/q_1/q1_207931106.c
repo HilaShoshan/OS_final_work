7d019599a3a2271c81fcad3f572319fa08d4fea1
Hila <hilashoshan0605@gmail.com>
2020-07-26 17:52:38 +0300

    determine where objects allocated using nm tool

---
+++ fwork_207931106/q_1/q1_207931106.c
@@ -0,0 +1,41 @@
+#define _BSD_SOURCE
+#include <stdio.h>
+#include <stdlib.h>
+
+char globBuf[65536];            /* 1. uninitialized data section (bss) */
+int primes[] = { 2, 3, 5, 7 };  /* 2. initialized data section */
+
+static int
+square(int x)                   /* 3. text (code) section */
+{
+    int result;                 /* 4. Where is allocated? */
+
+    result = x * x;
+    return result;              /* 5. How the return value is passed? */
+}
+
+static void
+doCalc(int val)                 /* 6. text (code) section */
+{
+    printf("The square of %d is %d\n", val, square(val));
+
+    if (val < 1000) {
+        int t;                  /* 7. Where is allocated? */
+
+        t = val * val * val;
+        printf("The cube of %d is %d\n", val, t);
+    }
+}
+
+int
+main(int argc, char* argv[])    /* text (code) section */
+{
+    static int key = 9973;      /* initialized data section */
+    static char mbuf[10240000]; /* uninitialized data section (bss) */
+    char* p;                    /* Where is allocated? */
+
+
+    doCalc(key);
+
+    exit(EXIT_SUCCESS);
+}

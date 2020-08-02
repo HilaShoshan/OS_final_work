85729972d61fc76ac4e81100371b7cc6855367f6
Hila <hilashoshan0605@gmail.com>
2020-08-02 21:11:05 +0300

    minor changes on code

--- fwork_207931106/q_2/q_21/check_pid.c
+++ fwork_207931106/q_2/q_21/check_pid.c
@@ -1,6 +1,7 @@
 # include <errno.h>
 # include <stdio.h>
 # include <signal.h>
+# include <stdlib.h>
 
 // program to check if a proccess (with spesific pid) exists
 // using kill(<pid>, 0) 
@@ -15,8 +16,10 @@ int main(int argc, char **argv) {
             printf("Process %d does not exist", pid);
         else if(errno == EPERM) 
             printf("Process %d exists but we have no permission", pid);
-        else
+        else {
             printf("call to kill() failed for some reason");
+            exit(1);
+        }
     }
     return 0; 
 }
--- fwork_207931106/q_2/q_22/client.c
+++ fwork_207931106/q_2/q_22/client.c
@@ -9,6 +9,10 @@ int main(int argc, char **argv) {
     sscanf(argv[1],"%d",&server_pid);
     sscanf(argv[2],"%d",&signal_num);
     sscanf(argv[3],"%d",&signal_count);
+    if(signal_num != 2 || signal_num != 10) {
+        printf("signal_num is illegal! \n please choose 2 for SIGINT or 10 for SIGUSR1");
+        exit(1);
+    }
     int i = 0;
     while(i < signal_count) {
         kill(server_pid, signal_num);
